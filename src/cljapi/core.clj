dcf667987514e6c88e6921b7f1b24918b6946024
Masanori Kinoshita <masa.kinoshita02@gmail.com>
2022-06-10 16:15:06 +0900

    Make it enable to run the Web server

--- src/cljapi/core.clj
+++ src/cljapi/core.clj
@@ -1,9 +1,16 @@
-(ns cljapi.core)
+(ns cljapi.core
+  (:require
+   [ring.adapter.jetty9 :as jetty]))
 
+(defn ring-handler
+  [_req]
+  {:status 200
+   :body "Hello, Clojure API"})
 
 (defn -main
   [& _args]
-  (println "Hello, Clojure API!"))
+  (jetty/run-jetty ring-handler {:port 8000}))
 
-
-(-main)
+(comment
+  (-main)
+  )
