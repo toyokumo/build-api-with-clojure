38b73e8f1d3bc57daa15c6d5f24df1db2f690488
Masanori Kinoshita <masa.kinoshita02@gmail.com>
2022-06-13 15:52:41 +0900

    Add system

---
+++ src/cljapi/system.clj
@@ -0,0 +1,28 @@
+(ns cljapi.system
+  (:require
+   [cljapi.component.handler :as c.handler]
+   [cljapi.component.server :as c.server]
+   [com.stuartsierra.component :as component]))
+
+(defn- new-system []
+  (component/system-map
+   :handler (c.handler/map->Handler {})
+   :server (component/using
+            (c.server/map->Jetty9Server {:opts {:join? false
+                                                :port 8000}})
+            [:handler])))
+
+(defn start []
+  (let [system (new-system)]
+    (component/start system)))
+
+(defn stop [system]
+  (component/stop system))
+
+(defonce system (atom nil))
+
+(defn go []
+  (when @system
+    (stop @system)
+    (reset! system nil))
+  (reset! system (start)))
