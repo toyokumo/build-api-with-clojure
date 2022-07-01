dd48f4e56d41c043c37e8b32fd9aca4c7061b914
Masanori Kinoshita <masa.kinoshita02@gmail.com>
2022-07-01 16:23:19 +0900

    Add logs to system

--- src/cljapi/system.clj
+++ src/cljapi/system.clj
@@ -3,7 +3,9 @@
    [cljapi.component.handler :as c.handler]
    [cljapi.component.server :as c.server]
    [cljapi.config :as config]
-   [com.stuartsierra.component :as component]))
+   [clojure.tools.logging :as log]
+   [com.stuartsierra.component :as component]
+   [unilog.config :as unilog]))
 
 (defn- new-system [config]
   (component/system-map
@@ -12,10 +14,17 @@
             (c.server/map->Jetty9Server (:server config))
             [:handler])))
 
+(defn- init-logging! [config]
+  (unilog/start-logging! (:logging config)))
+
 (defn start [profile]
   (let [config (config/read-config profile)
-        system (new-system config)]
-    (component/start system)))
+        system (new-system config)
+        _ (init-logging! config)
+        _ (log/info "system is ready to start")
+        started-system (component/start system)]
+    (log/info "system is started")
+    started-system))
 
 (defn stop [system]
   (component/stop system))
