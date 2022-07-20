b85d13d9eaece7e4fadbade9e14182e3e042e6bd
Masanori Kinoshita <masa.kinoshita02@gmail.com>
2022-07-20 16:58:32 +0900

    Use router created from reitit

--- src/cljapi/component/handler.clj
+++ src/cljapi/component/handler.clj
@@ -1,15 +1,16 @@
 (ns cljapi.component.handler
   (:require
-   [com.stuartsierra.component :as component]))
+   [cljapi.router :as router]
+   [com.stuartsierra.component :as component]
+   [reitit.ring :as ring]))
 
-(defn- ring-handler
-  [_req]
-  {:status 200
-   :body "Hello, Clojure API"})
+(defn- build-handler
+  []
+  (ring/ring-handler router/router))
 
 (defrecord Handler [handler]
   component/Lifecycle
   (start [this]
-    (assoc this :handler ring-handler))
+    (assoc this :handler (build-handler)))
   (stop [this]
     (assoc this :handler nil)))
