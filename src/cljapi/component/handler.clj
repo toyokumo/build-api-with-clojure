3e1719b3a82a01a68d4e6d43cc94f64767c44005
Masanori Kinoshita <masa.kinoshita02@gmail.com>
2022-07-26 10:35:06 +0900

    Add ring-logger

--- src/cljapi/component/handler.clj
+++ src/cljapi/component/handler.clj
@@ -5,21 +5,28 @@
    [cljapi.router :as router]
    [com.stuartsierra.component :as component]
    [reitit.ring :as ring]
+   [ring.logger :as m.logger]
    [ring.middleware.lint :as m.lint]
    [ring.middleware.reload :as m.reload]
    [ring.middleware.stacktrace :as m.stacktrace]))
 
+(def ^:private dev-middlewares
+  "開発時だけ有効化する"
+  [[m.reload/wrap-reload {:dirs ["src"]
+                          :reload-compile-errors? true}]
+   m.lint/wrap-lint
+   [m.stacktrace/wrap-stacktrace {:color? true}]])
+
 (defn- build-handler
   [profile]
-  (ring/ring-handler
-   router/router
-   nil
-   {:middleware (if (= profile :prod)
-                  []
-                  [[m.reload/wrap-reload {:dirs ["src"]
-                                          :reload-compile-errors? true}]
-                   m.lint/wrap-lint
-                   [m.stacktrace/wrap-stacktrace {:color? true}]])}))
+  (let [common-middlewares [m.logger/wrap-with-logger]
+        middlewares (if (= profile :prod)
+                      common-middlewares
+                      (apply conj dev-middlewares common-middlewares))]
+    (ring/ring-handler
+     router/router
+     nil
+     {:middleware middlewares})))
 
 (defrecord Handler [handler profile]
   component/Lifecycle
