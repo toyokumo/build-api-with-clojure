5b91e32a3bef89147808c8b867744aee2d920ce4
Masanori Kinoshita <masa.kinoshita02@gmail.com>
2022-07-26 11:04:57 +0900

    Add ring-defaults

--- src/cljapi/router.clj
+++ src/cljapi/router.clj
@@ -1,13 +1,20 @@
 (ns cljapi.router
   (:require
    [cljapi.handler :as h]
-   [reitit.ring :as ring]))
+   [reitit.ring :as ring]
+   [ring.middleware.defaults :as m.defautls]))
+
+(def ^:private ring-defaults-config
+  (-> m.defautls/api-defaults
+      ;; ロードバランサーの後ろで動いていると想定して、
+      ;; X-Forwarded-For と X-Forwarded-Proto に対応させる
+      (assoc :proxy true)))
 
 (def router
   (ring/router
    [["/health" {:name ::health
                 :handler h/handler}]
-    ["/api"
+    ["/api" {:middleware [[m.defautls/wrap-defaults ring-defaults-config]]}
      ["/hello" {:name ::hello
                 :handler h/handler}]
      ["/goodbye" {:name ::goodbye
